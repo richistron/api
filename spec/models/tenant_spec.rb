7d7d611a9c6a3c284a7c87bb98cb7d2bc2652de0
Ricardo Rivas <richistron@gmail.com>
2018-06-17 23:55:53 -0500

    tenant validates name

--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Tenant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Tenant' do
+    it 'should create a new tenant' do
+      tenant = build :tenant
+      expect(tenant.valid?).to eq(true)
+    end
+  end
 end
