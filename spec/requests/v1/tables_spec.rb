6b82667272629bb0813f99b79ee8f6e80ffc43bb
Ricardo Rivas <richistron@gmail.com>
2018-06-24 16:26:15 -0500

    better tests

--- spec/requests/v1/tables_spec.rb
+++ spec/requests/v1/tables_spec.rb
@@ -1,17 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "V1::Tables", type: :request do
-
-  def get_tenant
-    Tenant.first || create(:tenant)
-  end
-
-  let(:table) {
-    create :table, tenant: get_tenant
-  }
+RSpec.describe 'V1::Tables', type: :request do
 
   let(:session) {
-    user = create :user, tenant: get_tenant
+    tenant = create :tenant
+    user = create :user, tenant: tenant
     params = {
         email: user.email,
         password: user.password
@@ -24,61 +17,112 @@ RSpec.describe "V1::Tables", type: :request do
     }
   }
 
-  it 'index 200' do
-    get v1_tables_path, headers: session
-    expect(response).to have_http_status(200)
+  before :each do
+    session
+    # puts "session hash #{ session }"
   end
 
-  it 'index 401' do
-    get v1_tables_path
-    expect(response).to have_http_status(401)
-  end
+  describe 'V1::Table.index' do
 
-  it 'show 200' do
-    get v1_table_path(table), headers: session
-    expect(response).to have_http_status(200)
-  end
+    it '200 status' do
+      get v1_tables_path, headers: session
+      expect(response).to have_http_status(200)
+    end
 
-  it 'show 401' do
-    get v1_table_path(table)
-    expect(response).to have_http_status(401)
-  end
+    it 'format' do
+      get v1_tables_path, headers: session
+      res = JSON.parse response.body
+      expect(res).to eq([])
+    end
 
-  it 'create 201' do
-    post v1_tables_path, params: { table: {
-        name: 'yolo'
-    }}, headers: session
-    expect(response).to have_http_status(201)
-  end
+    it 'one element' do
+      tenant = Tenant.first
+      create :table, tenant: tenant
+      get v1_tables_path, headers: session
+      res = JSON.parse response.body
+      expect(res.first).to have_key('id')
+      expect(res.first).to have_key('name')
+      expect(res.first).to have_key('tenant_id')
+      expect(res.first).to have_key('created_at')
+      expect(res.first).to have_key('updated_at')
+    end
 
-  it 'create 401' do
-    post v1_tables_path, params: { table: {
-        name: 'yolo'
-    }}
-    expect(response).to have_http_status(401)
-  end
-
-
-  it 'update 200' do
-    tenant = create :tenant
-    table = create :table, tenant: tenant
-    patch v1_table_path(table), params: { table: {name: 'foo'}}, headers: session
-    expect(response).to have_http_status(200)
-  end
-
-  it 'update 401' do
-    tenant = create :tenant
-    table = create :table, tenant: tenant
-    patch v1_table_path(table), params: { table: {name: 'foo'}}
-    expect(response).to have_http_status(401)
+    it 'index 401' do
+      get v1_tables_path
+      expect(response).to have_http_status(401)
+    end
   end
 
 
-  it 'destroy' do
-    skip 'destoy is not implemented'
-    table = create :table
-    delete v1_table_path(table), params: { table: {name: 'foo'}}
-    expect(response).to have_http_status(200)
+
+
+  describe 'V1::Tables.show' do
+    it 'show 200' do
+      tenant = Tenant.first
+      table = create :table, tenant: tenant
+      get v1_table_path(table), headers: session
+      expect(response).to have_http_status(200)
+    end
+
+    it 'show 401' do
+      tenant = Tenant.first
+      table = create :table, tenant: tenant
+      get v1_table_path(table)
+      expect(response).to have_http_status(401)
+    end
+
+    it 'format' do
+      tenant = Tenant.first
+      table = create :table, tenant: tenant
+      get v1_table_path(table), headers: session
+      res = JSON.parse response.body
+      expect(res).to have_key('id')
+      expect(res).to have_key('name')
+      expect(res).to have_key('tenant_id')
+      expect(res).to have_key('created_at')
+      expect(res).to have_key('updated_at')
+    end
+  end
+
+  describe 'V1::Tables.create' do
+    it 'create 201' do
+      post v1_tables_path, params: { table: {
+          name: 'yolo'
+      }}, headers: session
+      expect(response).to have_http_status(201)
+    end
+
+    it 'create 401' do
+      post v1_tables_path, params: { table: {
+          name: 'yolo'
+      }}
+      expect(response).to have_http_status(401)
+    end
+  end
+
+  describe 'V1::Tables.destroy' do
+    it 'update 200' do
+      tenant = Tenant.first
+      table = create :table, tenant: tenant
+      patch v1_table_path(table), params: { table: {name: 'foo'}}, headers: session
+      expect(response).to have_http_status(200)
+    end
+
+    it 'update 401' do
+      tenant = Tenant.first
+      table = create :table, tenant: tenant
+      patch v1_table_path(table), params: { table: {name: 'foo'}}
+      expect(response).to have_http_status(401)
+    end
+  end
+
+  describe 'V1::Tables.destroy' do
+    it 'destroy' do
+      skip 'destoy is not implemented'
+      table = create :table
+      delete v1_table_path(table), params: { table: {name: 'foo'}}
+      expect(response).to have_http_status(200)
+    end
   end
 
 end
